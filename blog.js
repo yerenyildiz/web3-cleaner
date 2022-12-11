290102551c2c321ad6233cee211fd9daffc1104a
yerenyildiz <yerenyildiz@gmail.com>
2022-12-12 01:14:53 +0300

    resumewithsomeresponsivesettings

--- blog.js
+++ blog.js
@@ -24,27 +24,3 @@ EnglishButton.addEventListener("click", () => {
     EnglishContent.classList.remove("hidden");
     TurkishIsNotDefaultLang.style.display ="none";
     })
-
-    /////codewars
-/*Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. 
-    The string can contain any char.
-
-    Examples input/output:
-
-    XO("ooxx") => true
-    XO("xooxx") => false
-    XO("ooxXm") => true
-    XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
-    XO("zzoo") => false
-
-*/
-
-function XO(str) {
-    let exes = str.match(/x/gi)
-    let osss = str.match(/o/gi)
-    
-    if (exes !== null && osss !== null){
-    if (exes.length === osss.length) {return true} else {return false}; 
-} if (exes === null && osss === null){return true} else {return false}}
-
-console.log(XO("zzoo"))
